540e1ba3e86ad0f069b2d2567948af8a49f7ae06
AhnSeongHyun <sh84.ahn@gmail.com>
2015-04-28 23:28:51 +0900

    modify install.py for supporting python3

--- install.py
+++ install.py
@@ -4,45 +4,36 @@ Created on 2014. 12. 03
 @author: AhnSeongHyun
 '''
 
-
 import os
 import shutil
-
 import platform
 is_windows = False
 if str.lower(platform.uname()[0]) == "windows":
     is_windows = True
 
+print("\nWelcome slate-flask v0.1.")
+print("Start your API Document system.")
 
-def yellow(msg):
-    if is_windows:
-        return msg
-    else:
-        return "\033[1;33m" + msg + "\033[1;m"
+project_name = None
+try:
+    #  python2.x
+    project_name = str(raw_input('\nTyping API document name :'))
+except NameError:
+    #  python3.x
+    project_name = str(input('\nTyping API document name :'))
 
-
-def red(msg):
-    if is_windows:
-        return msg
-    else:
-        return "\033[1;41m" + msg + "\033[1;m"
-
-print yellow("\nWelcome slate-flask v0.1.")
-print yellow("Start your API Document system.")
-
-project_name = str(raw_input(yellow('\nTyping API document name :')))
-print "what is API document name? is" + red("\"" + project_name + "\"") + "."
+print("what is API document name? is" + "\"" + project_name + "\"" + ".")
 
 project_path = "../" + project_name
 
-print "Rename slate-flask to  " + red("\"" + project_name + "\"") + "..."
+print("Rename slate-flask to  " + "\"" + project_name + "\"" + "...")
 current_dir = os.path.abspath("./")
 dest_dir = os.path.join(os.path.dirname(current_dir), project_name)
 shutil.move(current_dir, dest_dir)
 
 from pip.req import parse_requirements
 import pip
-print yellow("Install requirements.txt")
+print("Install requirements.txt")
 
 install_reqs = parse_requirements("./requirements.txt")
 reqs = [str(ir.req) for ir in install_reqs]
@@ -50,5 +41,4 @@ for req in reqs:
     pip.main(["install", req])
     pip.logger.consumers = []
 
-
-print yellow("Complete. Enjoy Developing.")
+print("Complete. Enjoy Developing.")
