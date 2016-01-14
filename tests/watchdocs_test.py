2982432deae75fba0ae4c0b863d8baa3563d7ce6
sh.ahn <sh84.ahn@gmail.com>
2016-01-14 18:25:41 +0900

    etc test

--- tests/watchdocs_test.py
+++ tests/watchdocs_test.py
@@ -1,8 +1,35 @@
 import unittest
 
+from plate.watchdocs import DocumentTraceQueue
+from plate.watchdocs import DocumentTraceHandler
+from plate.watchdocs import APIDocumentObserver
 
-class WatchDocsTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class DocumentTraceQueueTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_is_empty(self):
+        pass
+
+    def count(self):
+        pass
+
+    def test_clear(self):
+        pass
+
+    def test_enqueue(self):
+        pass
+
+    def test_dequeue(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+class DocumentTraceHandlerTestCase(unittest.TestCase):
+    pass
 
 
+class APIDocumentObserverTestCase(unittest.TestCase):
+    pass
