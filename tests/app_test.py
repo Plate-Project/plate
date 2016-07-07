7bbff82d0e8c47dd51a492cdac5f733a85178398
ash84 <sh84.ahn@gmail.com>
2016-07-08 07:24:48 +0900

    add testcase for start test server and start service server

--- tests/app_test.py
+++ tests/app_test.py
@@ -14,8 +14,21 @@ class AppTestCase(unittest.TestCase):
         pass
 
     def test_configure_app(self):
-        pass
 
+        test_conf = {'KEY': 1234, 'db': 'localhost:3306//mc.dbo'}
+        import json
+        with open("./test.conf", 'w') as f:
+            f.write(json.dumps(test_conf))
+
+        from flask import Flask
+        from plate.common.config import Config
+        config = Config.load_conf('./test.conf')
+        app = Flask(__name__, static_url_path="", static_folder="static")
+        configure_app(app=app, config=config)
+
+        self.assertNotEqual(app, None)
+        self.assertEqual(app.config['KEY'], 1234)
+        self.assertEqual(app.config['db'], 'localhost:3306//mc.dbo')
 
     def test_configure_buleprints(self):
         from flask import Flask, Blueprint
@@ -31,10 +44,25 @@ class AppTestCase(unittest.TestCase):
         self.assertEqual(app.blueprints['page2'], page2)
         self.assertEqual(app.blueprints['page3'], page3)
 
-
     def test_start_test_server(self):
-        pass
+        from flask import Flask
+        app = Flask(__name__, static_url_path="", static_folder="static")
 
+        start_test_server(app)
+        r = requests.get('http://localhost:5000')
+        self.assertEqual(r.status_code, 200)
 
     def test_start_service_server(self):
-        pass
+        from flask import Flask
+        app = Flask(__name__, static_url_path="", static_folder="static")
+
+        start_service_server(app)
+        r = requests.get('http://localhost:8080')
+        self.assertEqual(r.status_code, 200)
+
+    def tearDown(self):
+        from os.path import exists
+        from os import remove
+
+        if exists('./test.conf'):
+            remove('./test.conf')
