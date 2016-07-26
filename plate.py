2ba78f81aaea02a02f4620e7de867327d51faa25
AhnSeongHyun <sh84.ahn@gmail.com>
2016-07-26 20:09:24 +0900

    Remote duplicated codes

--- plate.py
+++ plate.py
@@ -24,24 +24,10 @@ def parse_argument():
         sys.exit()
 
 
-def test_mode(config):
-    app = create_app(config=config)
-    from plate.watchdocs import APIDocumentObserver
-    api_doc_observer = APIDocumentObserver(doc_path=app.config['API_DOC_PATH'],
-                                           doc_index_path=app.config['API_DOC_INDEX_PATH'],
-                                           doc_file_path_list=api_doc.toc['ORDER'])
-    api_doc_observer.start_watch()
+def test_mode(app):
     start_test_server(app=app, port=app.config['PORT'])
 
-
-def service_mode(config):
-    app = create_app(config=config)
-    from plate.watchdocs import APIDocumentObserver
-    api_doc_observer = APIDocumentObserver(doc_path=app.config['API_DOC_PATH'],
-                                           doc_index_path=app.config['API_DOC_INDEX_PATH'],
-                                           doc_file_path_list=api_doc.toc['ORDER'])
-
-    api_doc_observer.start_watch()
+def service_mode(app):
     start_service_server(app=app, port=app.config['PORT'])
 
 
@@ -54,10 +40,16 @@ if __name__ == '__main__':
     # create documents
     from plate.api_document import APIDocument
     api_doc = APIDocument(config)
-
+ 
+    app = create_app(config=config)
+    from plate.watchdocs import APIDocumentObserver
+    api_doc_observer = APIDocumentObserver(doc_path=app.config['API_DOC_PATH'],
+                                           doc_index_path=app.config['API_DOC_INDEX_PATH'],
+                                           doc_file_path_list=api_doc.toc['ORDER'])
+    api_doc_observer.start_watch()
     if m == 'test':
-        test_mode(config=config)
+        test_mode(app=app)
     elif m == 'run':
-        service_mode(config=config)
+        service_mode(app=app)
     else:
         raise Exception("Not valid mode")
